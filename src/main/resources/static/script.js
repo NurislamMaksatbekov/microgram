3f7ac433c5b3ab78223a9ad3cb1b2c1e8c847781
Nurislam <maksatbekkov@icloud.com>
2023-04-10 18:37:39 +0600

    hw has done!

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -12,6 +12,7 @@ const post = {
     id: 1,
     image: 'beautifulimage',
     description: 'description',
+    timeOfPost: new Date(),
     like: 0,
     userId: 1, // я добавил в него userId. так и я связал его с пользователем
 
@@ -20,7 +21,7 @@ const post = {
 const comment = {
     id: 1,
     textOfComment: 'Cool bro',
-    timeOfComment: date(),
+    timeOfComment: new Date(),
     userId: 1,
     postId: 1 // также я добавил id пользователя и поста чтобы саязять их
 
@@ -46,14 +47,92 @@ function changePostLikedStatus(postId) {
     }
 }
 
-
+// TASK1
 function showSplashScreen(){
-    content.hidden = true;
+    const content = document.createElement('div')
+    content.classList.add()
+    document.body.appendChild(content)
 }
 
 function hideSplashScreen(){
-    content.hidden = false;
+    const content = document.getElementById('div')
+    content.classList.remove();
 }
+
+
+
+// TASK 2
+
+function createCommentElement(comment) {
+    const commentElement = document.createElement('div');
+    commentElement.classList.add();
+
+    const textElement = document.createElement('p');
+    textElement.classList.add();
+    textElement.textContent = comment.textOfComment;
+
+    const timeElement = document.createElement('span');
+    timeElement.classList.add();
+    timeElement.textContent = comment.timeOfComment;
+
+    const userElement = document.createElement('span');
+    userElement.classList.add();
+    userElement.textContent = comment.userId;
+
+    commentElement.appendChild(textElement);
+    commentElement.appendChild(timeElement);
+    commentElement.appendChild(userElement);
+
+    document.body.appendChild(commentElement)
+
+    return commentElement;
+}
+
+
+// TASK 3
+
+function createPostElement(post) {
+    const postElement = document.createElement('div');
+
+    const imageElement = document.createElement('img');
+    imageElement.src = post.image;
+
+    const descriptionElement = document.createElement('p');
+    descriptionElement.textContent = post.description;
+
+    const timeElement = document.createElement('span');
+    timeElement.textContent = post.timeOfPost;
+
+    const userElement = document.createElement('span');
+    userElement.textContent = post.userId;
+
+    const commentElement = document.createElement('div');
+    commentElement.classList.add();
+
+
+    postElement.appendChild(imageElement);
+    postElement.appendChild(descriptionElement);
+    postElement.appendChild(timeElement);
+    postElement.appendChild(userElement);
+    postElement.appendChild(commentElement);
+
+    document.body.appendChild(postElement)
+
+    return postElement;
+}
+
+// TASK4
+
+function addPost(postElement) {
+    const postsContainer = document.getElementById('post');
+    postsContainer.appendChild(postElement);
+
+    document.body.appendChild(postsContainer)
+}
+
+
+
+
 
 
 
